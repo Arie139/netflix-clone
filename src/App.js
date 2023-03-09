3f1bd30ace02e59d4b22cb206aebc2b9ce83b985
Ariel <90644357+Arie139@users.noreply.github.com>
2023-03-08 21:49:15 -0500

    added rows of movies with images, titles and like icon

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 
 function App() {
-	console.log(process.env.REACT_APP_IMDB_API_KEY);
 	return (
 		<>
 			<Navbar />
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Movie from './Movie';
 
-const Row = ({ title, fetchUrl }) => {
+const Row = ({ title, fetchURL }) => {
+	const [movies, setMovies] = useState([]);
+
+	useEffect(() => {
+		axios.get(fetchURL).then((response) => {
+			setMovies(response.data.results);
+		});
+	}, [fetchURL]);
+
 	return (
 		<>
 			<h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
+			<div className='relative flex items-center'>
+				<div
+					id={'slider'}
+					className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide'
+				>
+					{movies.map((item, id) => (
+						<Movie key={id} item={item} />
+					))}
+				</div>
+			</div>
 		</>
 	);
 };
